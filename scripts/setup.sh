467f280504c97200d2a8506e6cb940ad63e75d36
David Rolfe <drolfe@voltdb.com>
2021-03-24 17:41:06 +0000

    Remove topics changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,3 @@ cd voltdb-cvmsandbox/ddl
 
 sqlcmd --servers=vdb1 < create_db.sql
 
-java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-cvmsandbox/scripts/export_and_import.xml
