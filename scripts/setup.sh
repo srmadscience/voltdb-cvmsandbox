33383b3f8b3e4f552ecaa1b59f8a01b6d014dcd9
dwrolfe <david.rolfe@pobix.com>
2025-05-12 18:07:38 +0100

    added dashboard

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,10 @@
 
 . $HOME/.profile
 
+USERCOUNT=4000000
+
 cd
+mkdir logs 2> /dev/null
 cd voltdb-cvmsandbox/ddl
 
 sqlcmd --servers=vdb1 < create_db.sql
@@ -10,5 +13,11 @@ sqlcmd --servers=vdb1 < create_db.sql
 cd
 cd voltdb-cvmsandbox/scripts
 
-java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment topics.xml
+java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1 deployment topics.xml
 
+$HOME/bin/reload_dashboards.sh voltdb-cvmsandbox.json
+
+java  ${JVMOPTS}  -jar $HOME/bin/addtodeploymentdotxml.jar `cat $HOME/.vdbhostnames`  deployment $HOME/voltdb-voltdb-cvmsandbox/scripts/export_and_import.xml
+
+cd ../jars
+java ${JVMOPTS} -jar CreateChargingDemoData.jar `cat $HOME/.vdbhostnames`  $USERCOUNT 30 100000
